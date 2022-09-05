98647dbddaed5c0d2b5b63849c6f398c6975b3a5
maneesh <maneesh@maneesh-Lenovo-E41-25>
2022-09-05 11:31:04 +0530

    Output

---
+++ src/basic/output.java
@@ -0,0 +1,19 @@
+package basic;
+
+public class output {
+	public static void main(String[] args) {
+		int a=3;
+		int b=5;
+		if(a>2 && a++>2)
+		{
+			++b;
+		}
+		else
+		{
+			a++;
+		}
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
