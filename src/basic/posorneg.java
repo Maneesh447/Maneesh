94d82eadef0f2da691cec7001f963518cbdc029c
maneesh <maneesh@maneesh-Lenovo-E41-25>
2022-09-07 20:01:53 +0530

    Positive r Negative

--- src/basic/posorneg.java
+++ src/basic/posorneg.java
@@ -2,20 +2,20 @@ package basic;
 
 public class posorneg {
 	public static void main(String[] args) {
-		int a=5;
-		if(a%2==0 && a<=0)
+		int a=9;
+		if(a%2==0 && a>0)
 		{
 			System.out.println("Positive Even Number");
 		}
-		else if(a%2<=0 && a>=0)
+		else if(a%2==0 && a<0)
 		{
 			System.out.println("Negative Even Number");
 		}
-		else if(!(a%2>=0 && a>=0))
+		else if(!(a%2==0) && (a<0))
 		{
 			System.out.println("Negative odd Number");
 		}
-		else if(!(a%2==0 && a>=0))
+		else if(!(a%2==0) && (a>0))
 		{
 			System.out.println("Positive Odd Number");
 		}
