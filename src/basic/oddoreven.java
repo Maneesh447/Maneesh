e205addf056347e50b6444351e234530c1dee543
maneesh <maneesh@maneesh-Lenovo-E41-25>
2022-09-07 20:02:25 +0530

    Odd r Even

---
+++ src/basic/oddoreven.java
@@ -0,0 +1,16 @@
+package basic;
+
+public class oddoreven {
+	public static void main(String[] args) {
+	int a=5;
+	int b=a%2;
+	if(b==0)
+	{
+		System.out.print("Even Number");
+	} else
+	{
+		System.out.print("Odd Number");
+	}
+	}
+
+}
