865e5cfd78525c99435732b9fdabea47f855d74b
maneesh <maneesh@maneesh-Lenovo-E41-25>
2022-09-07 20:02:53 +0530

    For Loop

---
+++ src/basic/forloop.java
@@ -0,0 +1,16 @@
+package basic;
+
+public class forloop {
+	public static void main(String[] args) {
+		int sum=0;
+		int a=2;
+		for(int i=1;i<=10;i++)
+		{
+			sum=i*a;
+			System.out.println(i+"*"+a+"="+sum);
+			//System.out.println(sum);
+		}
+			
+	}
+
+}
