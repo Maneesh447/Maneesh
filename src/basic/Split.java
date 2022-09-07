af7da1a00d90d4864ca6a3d564e1127ccd74c8e5
maneesh <maneesh@maneesh-Lenovo-E41-25>
2022-09-07 19:57:51 +0530

    Spilt

---
+++ src/basic/Split.java
@@ -0,0 +1,33 @@
+package basic;
+
+public class Split {
+	public static void main(String[] args) {
+		int num=79863;
+		if(num>0) {
+			int a=num%10;
+			System.out.println(a);
+			num=num/10;
+		}
+		if(num>0) {
+			int a=num%10;
+			System.out.println(a);
+			num=num/10;
+		}
+		if(num>0) {
+			int a=num%10;
+			System.out.println(a);
+			num=num/10;
+		}
+		if(num>0) {
+			int a=num%10;
+			System.out.println(a);
+			num=num/10;
+		}
+		if(num>0) {
+			int a=num%10;
+			System.out.println(a);
+			num=num/10;
+		}
+	}
+
+}
