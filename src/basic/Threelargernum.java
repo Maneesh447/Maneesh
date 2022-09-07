9592be7d6f446393b3b32895b8edacc644691c20
maneesh <maneesh@maneesh-Lenovo-E41-25>
2022-09-07 19:59:23 +0530

    Three larger Number Two Larger Number

---
+++ src/basic/Threelargernum.java
@@ -0,0 +1,24 @@
+package basic;
+
+public class Threelargernum {
+		public static void main(String[] args) {
+			int a=17;
+			int b=19;
+			int c=15;
+			if (a>b && a>c)
+			{
+				System.out.print("A is larger Number");
+			}
+			else if(b>c)
+			{
+				System.out.print("B is larger Number");
+			}
+			else
+			{
+				System.out.print("C is Larger Number");
+			}
+
+	}
+
+
+}
---
+++ src/basic/twolargernum.java
@@ -0,0 +1,17 @@
+package basic;
+
+public class twolargernum {
+	public static void main(String[] args) {
+		int a=5;
+		int b=8;
+		if(a>b)
+		{
+			System.out.print("A is larger Number");
+		}
+		else
+		{
+			System.out.print("B is larger Number");
+		}
+	}
+
+}
