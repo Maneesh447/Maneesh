ae1da09f38fb771cc133739c88a354042f03e8ec
maneesh <maneesh@maneesh-Lenovo-E41-25>
2022-09-08 19:58:55 +0530

    Divisibleby 3 or 5

---
+++ src/basic/Divisibleby3or5.java
@@ -0,0 +1,15 @@
+package basic;
+
+public class Divisibleby3or5 {
+	public static void main(String[] args) {
+		int n=1;
+		for( ;n<=20;n++)
+		{
+			if(n%3==0 || n%5==0) {
+				//System.out.println(i);
+			    System.out.println(n);
+			}
+		}
+			
+	}
+}
