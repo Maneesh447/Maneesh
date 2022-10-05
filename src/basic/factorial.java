43f2ff43946c70e23bcedcb0c17c9d50ae0ad3e8
maneesh <maneesh@maneesh-Lenovo-E41-25>
2022-10-05 16:44:32 +0530

    factorial

---
+++ src/basic/factorial.java
@@ -0,0 +1,14 @@
+package basic;
+
+public class factorial {
+	public static void main(String[] args) {
+		int fact=2;
+		for(int i=1;i<=5;i++) 
+		{
+			fact=fact*i;
+		}
+		System.out.println(fact);
+	}
+	
+
+}
