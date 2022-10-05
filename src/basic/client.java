a81fdb90aebb3cacba687dadcdde79d26e2b4fa6
maneesh <maneesh@maneesh-Lenovo-E41-25>
2022-10-05 16:44:05 +0530

    client

---
+++ src/basic/client.java
@@ -0,0 +1,18 @@
+package basic;
+
+public class client {
+	private void clientName() {
+		System.out.println("Bhuvi");
+	}
+	
+	private void clientId() {
+		System.out.println("250704");
+	}
+	
+public static void main(String[] args) {
+	client s=new client();
+	s.clientName();
+	s.clientId();
+}
+}
+
