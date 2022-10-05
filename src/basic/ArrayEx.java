3afdef745ecf7d14c26d6f0b0c284b53df03896b
maneesh <maneesh@maneesh-Lenovo-E41-25>
2022-10-05 16:38:37 +0530

    ArrayEx

---
+++ src/basic/ArrayEx.java
@@ -0,0 +1,18 @@
+package basic;
+
+public class ArrayEx {
+	public static void main(String[] args) {
+		int a[]= {10,20,30,40,50};
+		for(int i=0;i<a.length;i++)
+			//for(int i=a.length-1;i>=0;i--)
+		//int a[]=new int[10];
+		{
+			//a[0]=10;
+			//a[1]=20;
+			//a[3]=30;
+			//a[4]=50;
+		System.out.println(a[i]);
+		}
+	}
+
+}
