42c47ef0680667cdc3c2f5608afa5d5e3f3959ec
maneesh <maneesh@maneesh-Lenovo-E41-25>
2022-10-07 23:17:16 +0530

    mul3and5only

---
+++ src/Assignment/mul3and5only.java
@@ -0,0 +1,24 @@
+package Assignment;
+
+public class mul3and5only {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    for(int i=1;i<=15;i++) 
+	    {
+		if(i%3==0)
+		{
+			System.out.println(i);
+		}
+	    }
+	    System.out.println("");
+	    for(int i=1;i<=25;i++) 
+	    {
+		if(i%5==0)
+		{
+			System.out.println(i);
+		}
+	    }
+	}
+
+}
